0c1e073dea022df3e05b522d4f8f092c3d0a968a
Jethro Carr <jethro.carr@jethrocarr.com>
2016-06-19 01:33:04 +1200

    Crude integration with beanstalk added

--- src/main/java/com/jethrocarr/howalarming/gcmserver/HowAlarmingServer.java
+++ src/main/java/com/jethrocarr/howalarming/gcmserver/HowAlarmingServer.java
@@ -20,7 +20,10 @@ package com.jethrocarr.howalarming.gcmserver;
 import com.google.gson.*;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
+
 import org.jivesoftware.smack.util.StringUtils;
+
+import com.dinstone.beanstalkc.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -38,10 +41,16 @@ import java.util.logging.Logger;
  */
 public class HowAlarmingServer {
 
-  // FriendlyGcmServer defines onMessage to handle incoming friendly ping messages.
-  private class FriendlyGcmServer extends GcmServer {
 
-    public FriendlyGcmServer (String apiKey, String senderId, String serviceName) {
+  // MARK: GcmServer
+
+  /**
+   * We define our own specific GcmServer in order to override onMessage with specific logic for handling the messages
+   * we receive from the mobile apps via the GCM network.
+   */
+  private class HowAlarmingGcmServer extends GcmServer {
+
+    public HowAlarmingGcmServer (String apiKey, String senderId, String serviceName) {
       super(apiKey, senderId, serviceName);
     }
 
@@ -72,12 +81,8 @@ public class HowAlarmingServer {
           case "fire":
           case "medical":
           case "police":
-            // Supported simple commands in HowAlarming.
-
-            // TODO: Feed into beanstalk queue.
-
-            // TODO: test by sending message
-            messageAllClients();
+            // Supported simple commands in HowAlarming. We feed these into the beanstalk queue(s)
+            beanstalkClient.beanstalkPost(command);
           break;
 
           default:
@@ -91,11 +96,92 @@ public class HowAlarmingServer {
     }
   }
 
+
+  // MARK: BeanstalkClient
+
+  /*
+    BeanstalkClient defines a class for exchanging messages to/from Beanstalk
+   */
+  public class BeanstalkClient {
+
+    private Configuration beanstalkConfig;
+    private BeanstalkClientFactory beanstalkFactory;
+
+    public BeanstalkClient() {
+      // Connect to beanstalk queue
+      beanstalkConfig = new Configuration();
+      beanstalkConfig.setServiceHost("127.0.0.1");
+      beanstalkConfig.setServicePort(11300);
+
+      beanstalkFactory = new BeanstalkClientFactory(beanstalkConfig);
+
+      // Launch the tube listener in a dedicated thread.
+      Thread beanstalkClientThread = new Thread(new BeanstalkListener());
+      beanstalkClientThread.setName("Beanstalk Queue Reader");
+      beanstalkClientThread.start();
+    }
+
+    /**
+     * The beanstalkPost method is called via the GCM server when a new (valid) command is received from a mobile
+     * device via GCM. It takes the message and pops it onto the queue(s) for the alarm to action.
+     */
+    public void beanstalkPost(String message) {
+      logger.info("Posting message to beanstalk:" + message);
+
+      JobProducer producer = beanstalkFactory.createJobProducer("cli");
+      producer.putJob(0, 0, 300, message.getBytes());
+
+    }
+
+
+    /**
+     * The listener runs long polls (60 seconds) in a loop waiting for any messages from the queue. Upon receiving a
+     * message, it broadcasts it to all registered clients via the GCM network.
+     */
+    public class BeanstalkListener implements Runnable {
+
+      public void run() {
+
+        JobConsumer consumer = beanstalkFactory.createJobConsumer("alert_gcm");
+
+        while (true) {
+          // Poll for new messages to process
+          Job job = consumer.reserveJob(60);
+
+          if (job != null) {
+            // Obtain the message and push to all clients
+            // TODO: I think we need to filter accepted messages here?
+            String message = new String(job.getData());
+            messageAllClients(message);
+
+            // Delete old message
+            consumer.deleteJob(job.getId());
+          }
+
+          logger.info("loop");
+        }
+
+        //consumer.close();
+      }
+    }
+  }
+
+
+
+  // MARK: Properties
+
+
   private static final Logger logger = Logger.getLogger("HowAlarmingServer");
 
   // Creds
   private static final String SERVER_API_KEY = System.getenv("GCM_API_KEY");
   private static final String SENDER_ID = System.getenv("GCM_SENDER_ID");
+
+  // Beanstalk Queue
+  private static final String BEANSTALK_HOST            = System.getenv("BEANSTALK_HOST");
+  private static final String BEANSTALK_PORT            = System.getenv("BEANSTALK_PORT");
+  private static final String BEANSTALK_TUBES_EVENTS    = System.getenv("BEANSTALK_TUBES_EVENTS");
+  private static final String BEANSTALK_TUBES_COMMANDS  = System.getenv("BEANSTALK_TUBES_COMMANDS");
 
 
   // Other
@@ -106,19 +192,28 @@ public class HowAlarmingServer {
   private List<String> registeredClients;
 
   // Listener responsible for handling incoming registrations and pings.
-  private FriendlyGcmServer friendlyGcmServer;
+  private HowAlarmingGcmServer HowAlarmingGcmServer;
+
+  // Beanstalk Client
+  private BeanstalkClient beanstalkClient;
 
   // Gson helper to assist with going to and from JSON and Client.
   private Gson gson;
 
 
+
+
+
+  // MARK: HowAlarming Server
+
+
   public HowAlarmingServer(String apiKey, String senderId) {
 
     registeredClients = new ArrayList<String>();
-
     gson = new GsonBuilder().create();
 
-    friendlyGcmServer = new FriendlyGcmServer(apiKey, senderId, SERVICE_NAME);
+    beanstalkClient = new BeanstalkClient();
+    HowAlarmingGcmServer = new HowAlarmingGcmServer(apiKey, senderId, SERVICE_NAME);
   }
 
 
@@ -136,7 +231,7 @@ public class HowAlarmingServer {
 
 
   /**
-   * Define the format of push messages to the device
+   * Data model for push messages to mobile devices via GCM
    */
 
   public class pushMessage {
@@ -160,14 +255,16 @@ public class HowAlarmingServer {
   /**
    * Deliver a message to all registered clients (basically broadcast, we don't need to care about 1-1 messaging)
    */
-  private void messageAllClients() {
+  public void messageAllClients(String message) {
+
+    // TODO we should use pushMessage class for this data?
 
     logger.info("Dispatching broadcast message to all registered clients");
 
     pushMessage myPushMessage = new pushMessage();
 
     myPushMessage.data.put("raw", "123abc");
-    myPushMessage.data.put("message", "test message");
+    myPushMessage.data.put("message", message);
     myPushMessage.data.put("type", "alarm");
     myPushMessage.data.put("code", "123");
     myPushMessage.data.put("timestamp", (String.valueOf((System.currentTimeMillis() / 1000L))));
@@ -181,9 +278,13 @@ public class HowAlarmingServer {
     JsonObject jData = new JsonParser().parse(messageString).getAsJsonObject();
 
     for (String clientToken : registeredClients) {
-      friendlyGcmServer.send(clientToken, jData);
+      HowAlarmingGcmServer.send(clientToken, jData);
     }
   }
+
+
+
+  // MARK: main()
 
   public static void main(String[] args) {
 
